fce577b36bd6b5a2c1e26cd5adaaf5676df8d4b9
Michael Kevin <michaelkmb@hotmail.com>
2023-09-19 11:11:22 -0300

    Spring Security Hello World Example;

---
+++ spring-security/src/test/java/edu/example/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.example.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
